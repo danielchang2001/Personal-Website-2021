a86223bcd6ba14bcd3ab7ae1db163f6c776ede21
danielchang2001 <danielchang083@gmail.com>
2021-05-30 23:01:14 -0700

    When a nav element is pressed, navigation closes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
-// OPEN NAV MENU
-
+// Navigation Variables
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
+// Open Navigation
 if (navToggle) {
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
+// Close Navigation
 if (navClose) {
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
+
+// Close Navigation When Nav Element Pressed
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
